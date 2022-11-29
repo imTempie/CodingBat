baafa9ead43ab526da07cd4cefcc45887d9e3fee
tempie <63066656+imTempie@users.noreply.github.com>
2022-11-29 11:14:23 +0000

    Update factorial.java

--- Recursion-1/factorial.java
+++ Recursion-1/factorial.java
@@ -1,3 +1,8 @@
+/*
+Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. 
+Compute the result recursively (without loops).
+*/
+
 public int factorial(int n) {
   
   //base case, if n = 1 then we return it
